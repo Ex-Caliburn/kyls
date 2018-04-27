9fe8df6e44f9290dde6fab2077d5c9e53174f2e5
lijiye <lijiye@jinddaka.com>
2018-04-27 21:37:03 +0800

    '院校列表更新'

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,6 +1,27 @@
-import { combineReducers } from 'redux'
-import counter from './counter'
+import { handleActions } from 'redux-actions'
 
-export default combineReducers({
-  counter
+export default handleActions({
+  'updateUSID' (state, action) {
+    return {
+      ...state,
+      undergraduateSchoolId: action.payload
+    }
+  },
+  'updateUMID' (state, action) {
+    return {
+      ...state,
+      undergraduateMajorId: action.payload
+    }
+  },
+  'updateVS' (state, action) {
+    return {
+      ...state,
+      asyncNum: action.payload
+    }
+  }
+}, {
+  undergraduateSchoolId: 0,
+  undergraduateMajorId: 0,
+  volunteerSchool: '', // 志愿学校
+  volunteerMajor: '' // 志愿专业
 })
